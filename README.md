f63d9b01cee1224fc750cf98b0c8344bc0fd071e
fikridivasambasri <fikridivasambasri21@gmail.com>
2020-09-17 10:55:05 +0700

    upload projek vue

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# ShaynaStoreVue
-Project web shayna store dengan vue js
+# vue-laravel-store-fikri
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="home">
+		<HeaderShayna />
+		<HeroShayna />
+		<BannerShayna />
+		<Instagram />
+		<PartnerLogo />
+		<Footer />
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import BannerShayna from "@/components/BannerShayna.vue";
+import Instagram from "@/components/Instagram.vue";
+import PartnerLogo from "@/components/PartnerLogo.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+	name: "Home",
+	components: {
+		HeaderShayna,
+		HeroShayna,
+		BannerShayna,
+		Instagram,
+		PartnerLogo,
+		Footer,
+	},
+};
+</script>
